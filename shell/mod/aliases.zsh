92c685ab6f927825eb2003b9d9e0c77fe1e6290c
Quentin Eagan <idontstartfires@gmail.com>
2026-07-20 10:48:25 -0400

    zen -> zen-browser, vi -> nvim

--- shell/mod/aliases.zsh
+++ shell/mod/aliases.zsh
@@ -2,7 +2,9 @@
 # Concise!
 alias pac='doas pacman'
 alias sys='doas systemctl'
+
 alias firefox='firefox-developer-edition'
+alias zen='zen-browser'
 
 # Modern replacements for classic commands
 alias ls='lsd'
@@ -10,6 +12,7 @@ alias cat='bat'
 alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:100 {}"'
 alias diff='delta'
 alias dog='doggo'
+alias vi="nvim"
 alias vim="nvim"
 
 # It's rare that I'd want to avoid making subdirectories...
